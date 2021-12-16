3438cb3b3f9908ddbad53c1f51a9995b898d214f
lucas venancio <lucasduarte@MacBook-Pro-de-lucas.local>
2021-12-16 20:34:01 -0300

    adicao de babel-plugin-root-import

---
+++ src/core/Text/Index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+function TextComponent() {
+  return (
+    <Text>Open up App.js to start working on your app!</Text>
+  );
+}
+
+export default TextComponent
